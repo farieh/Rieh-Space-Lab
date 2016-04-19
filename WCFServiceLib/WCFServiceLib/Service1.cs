a728c7b7c64280b174c311322392b6f1ebc6f577
farih <farieh87@yahoo.co.id>
2016-04-19 11:58:18 +0700

    Try dan test the very very begining of WCF

--- WCFServiceLib/WCFServiceLib/Service1.cs
+++ WCFServiceLib/WCFServiceLib/Service1.cs
@@ -10,9 +10,9 @@ namespace WCFServiceLib
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        public string GetData(string value)
         {
-            return string.Format("You entered: {0}", value);
+            return string.Format("{0}", value);
         }
 
         public CompositeType GetDataUsingDataContract(CompositeType composite)
